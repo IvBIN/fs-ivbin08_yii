fbdaaea878b872c81628527143a17cb9dca78882
avilovmaksim5 <avilovmaksim5@mail.ru>
2024-03-18 20:38:09 +0400

    update 18/03/24

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -25,12 +25,17 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'only' => ['logout'],
+                'only' => ['logout', 'about'],
                 'rules' => [
                     [
                         'actions' => ['logout'],
                         'allow' => true,
                         'roles' => ['@'],
+                    ],
+                    [
+                        'actions' => ['about'],
+                        'allow' => true,
+                        'roles' => ['admin'],
                     ],
                 ],
             ],
